d7ac858792f4bbac4259693edb657aa768ba5a59
MyUsername <alberto.baglivi@gmail.com>
2022-10-14 16:45:52 +0200

    add the palindrome Program

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,1 +1,36 @@
-alert("Ciao")
+
+let  word1 = prompt("Inserisci una parola:")
+
+if(isPal(word1)){
+    alert("la parola è palindroma")
+}
+else{
+    alert("la parola non è palindroma")
+}
+
+
+
+
+
+function isPal(word){
+
+    let str = word.toLowerCase()
+    let bool;  
+    let x;
+
+    for(let i =0 ; i < str.length/2 ; i++){
+             x = (str.length - 1) - i ; 
+            if(str[i] === str[x]){
+                bool = true
+            }else{
+                bool = false 
+            }
+    }
+
+return bool 
+}
+
+
+
+
+
